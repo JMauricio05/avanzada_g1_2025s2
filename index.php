cae04a69fa79e523ee50992554169b6e0f68042d
Sala D1 <CSalaD1@uniboyaca.edu.co>
2025-09-13 09:46:13 -0500

    include n php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+//require || include;
+include "metodos.php";
+
+$opr = new Operaciones();
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+</head>
+
+<body>
+    <h1>Inicio</h1>
+    <button id="btn">Ver sumas</button>
+    <div id="suma1" style="display:none;">
+        2 + 2 = <?php echo $opr->sumar(2, 2); ?>
+    </div>
+    <div>
+        2 * 2 = <?php echo $opr->multiplicar(2, 2); ?>
+    </div>
+
+    <div>
+        2 / 2 = <?php echo $opr->dividir(2, 2); ?>
+    </div>
+    <div>
+        2 / 0 = <?php echo $opr->dividir(2, 0); ?>
+    </div>
+
+    <script>
+        document.getElementById('btn').addEventListener('click', () => {
+            document.getElementById('suma1').style.display = 'block';
+        });
+    </script>
+</body>
+
+</html>
